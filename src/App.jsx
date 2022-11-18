21c608437862a13fe0883c7a8f9850a74bd84a5e
Johan Herrera <johanherrera20000@gmail.com>
2022-11-17 22:22:37 -0500

    Frontend Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+ import Header from "./components/Header"
+ import Form from "./components/Form"
+ import ListPatients from "./components/ListPatients"
+ 
+ function App() {
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mx-auto mt-20">
+      <Header/>
+
+      <div className="mt-12 flex flex-col md:flex-row">
+      <Form/>
+      <ListPatients/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+        <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+            Follow-Up Patients {``}
+            <span className="text-indigo-600">Veterinary</span>
+        </h1>
+    </>
+  )
+}
+
+export default Header
