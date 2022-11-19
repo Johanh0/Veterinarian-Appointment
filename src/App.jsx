94030b6ecd4dc2025f870c62c0ef796a0bd6a9a0
Johan Herrera <johanherrera20000@gmail.com>
2022-11-18 20:29:28 -0500

    Project is almost done but for some reason the localStorage it's not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
- import { useState } from "react";
+ import { useState, useEffect } from "react";
  import Header from "./components/Header"
  import Form from "./components/Form"
  import ListPatients from "./components/ListPatients"
@@ -6,6 +6,19 @@
  function App() {
   const [patients, setPatients] = useState([]);
   const [patient, setPatient] = useState({});
+
+  useEffect(() => {
+    const getLocalStorage = () => {
+      const patientsLocalStorage = JSON.parse(localStorage.getItem('patients')) ?? [];
+      setPatients(patientsLocalStorage);
+    }
+
+    getLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(`patients`, JSON.stringify(patients));
+  }, [patients]);
 
   const deletePatient = id => {
     const patientsUpdate = patients.filter(patient => patient.id !== id);
